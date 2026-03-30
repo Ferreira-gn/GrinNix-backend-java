8b7280013632d2711796f54d4373bdb9d133a34d
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2026-03-30 01:21:05 -0300

    add repository annotation

--- src/main/java/com/ferreira_gn/grannix/persistence/repositories/ProblemsRepository.java
+++ src/main/java/com/ferreira_gn/grannix/persistence/repositories/ProblemsRepository.java
@@ -3,8 +3,10 @@ package com.ferreira_gn.grannix.persistence.repositories;
 import com.ferreira_gn.grannix.persistence.entity.ProblemsEntity;
 import com.ferreira_gn.grannix.services.ProblemsModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProblemsRepository extends JpaRepository<ProblemsEntity, UUID> {
 }
