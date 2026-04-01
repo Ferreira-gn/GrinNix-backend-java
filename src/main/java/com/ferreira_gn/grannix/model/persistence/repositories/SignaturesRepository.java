aab2db720b16fc88ce63d32f1bbf2e9d20f9a030
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2026-03-31 21:48:59 -0300

    feat: Add signature return by language and issue.

--- src/main/java/com/ferreira_gn/grannix/model/persistence/repositories/SignaturesRepository.java
+++ src/main/java/com/ferreira_gn/grannix/model/persistence/repositories/SignaturesRepository.java
@@ -2,10 +2,21 @@ package com.ferreira_gn.grannix.model.persistence.repositories;
 
 import com.ferreira_gn.grannix.model.persistence.entity.SignaturesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SignaturesRepository extends JpaRepository<SignaturesEntity, UUID> {
+    @Query("""
+        SELECT s FROM SignaturesEntity s
+        INNER JOIN s.problem p
+        INNER JOIN s.language l
+        WHERE p.id = :problemId
+        AND l.id = :languageId
+    """)
+    Optional<SignaturesEntity> findByLanguageIdAndProblemId(@Param("languageId") UUID languageId, @Param("problemId") UUID problemId);
 }
